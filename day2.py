eb54b0f73c821e392a64106b1e3af31dca33abb6
jiabin713 <jiabin713@gmail.com>
2018-10-16 17:33:44 +0800

    day2

---
+++ day2.py
@@ -0,0 +1,8 @@
+# 【战胜拖延症，组团学 Python】准备工作（二）：
+# 1. 安装一个代码编辑器。（推荐 visual studio code ，安装 Python 插件）
+# 2. 自行准备教材「笨办法学 Python3」 作者：泽德 A. 肖（Zed A.Shaw）  。
+
+# 回复利用编辑器编写并运行以下代码的截图：
+
+company,date,*others = ['juejin','20181016','morning','Tues']
+print(company,date,others)
