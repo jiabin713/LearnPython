4ffb5901658e21427bb33807eba39b6c3581f16a
jiabin713 <jiabin713@gmail.com>
2018-10-17 21:43:10 +0800

    day3.py

---
+++ lpthw/ex1.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
+
+# 1. Make your script print another line.
+print('那我就再打一行吧')
+
+# 2. Make your script print only one of the lines.
+print('这句话末尾不换行', end=' ')
+print('mmmmm')
+
+# 3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what
+# this character does.
+
+# 井号是注释符, 不会编译代码
---
+++ lpthw/ex3.py
@@ -0,0 +1,59 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+
+# 1. Above each line, use the # to write a comment to yourself explaining what the line does.
+
+# 2. Remember in Exercise 0 when you started python3.6? Start python3.6 this way again and,
+
+# using the math operators, use Python as a calculator.
+
+# 3. Find something you need to calculate and write a new .py file that does it.
+
+# 4. Rewrite ex3.py to use floating point numbers so it’s more accurate. 20.0 is floating point.
+
+
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3.0 + 2.0?", 3.0 + 2.0)
+print("What is 5.0 - 7.0?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
